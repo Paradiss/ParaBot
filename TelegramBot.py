d3683317acc776750f7c82d2f43fda04794e5901
Aleksandr Efimov <esasha-05@yandex.ru>
2019-09-15 10:29:25 +0300

    Теперь можно получать созвездие марса

--- TelegramBot.py
+++ TelegramBot.py
@@ -1,6 +1,8 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import logging
 import settings
+import ephem
+from datetime import datetime, date, time
 
 logging.basicConfig(format='%(asktime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO,
@@ -17,14 +19,29 @@ def talk_to_me(bot, update):
     print(user_text)
     update.message.reply_text(user_text)
 
+def constellation(bot, update):
+    user_text = update.message.text 
+    user_text_list = user_text.split()
+    planet = user_text_list[1]
+    if planet.lower() == 'mars':
+        mars = ephem.Mars()  
+        mars.compute(datetime.today().strftime('%Y/%m/%d'))
+        print(ephem.constellation(mars))
+        update.message.reply_text(ephem.constellation(mars))
+
 def main():
     mybot = Updater(settings.API_KEY, request_kwargs=settings.PROXY)
 
     logging.info('Бот запускается')
 
+    mars = ephem.Mars()  
+    mars.compute(datetime.today().strftime('%Y/%m/%d'))
+    print(ephem.constellation(mars))
+
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler("start", greet_user))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
+    dp.add_handler(CommandHandler("planet", constellation))
 
     mybot.start_polling()
     mybot.idle()
