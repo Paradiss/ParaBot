5072635373ad3565283953d8ffbbcf81b59bb011
Aleksandr Efimov <esasha-05@yandex.ru>
2019-09-18 07:58:52 +0300

    Теперь астрологи предсказывают созвездие для любой планеты

--- TelegramBot.py
+++ TelegramBot.py
@@ -22,21 +22,28 @@ def talk_to_me(bot, update):
 def constellation(bot, update):
     user_text = update.message.text 
     user_text_list = user_text.split()
-    planet = user_text_list[1]
-    if planet.lower() == 'mars':
-        mars = ephem.Mars()  
-        mars.compute(datetime.today().strftime('%Y/%m/%d'))
-        print(ephem.constellation(mars))
-        update.message.reply_text(ephem.constellation(mars))
+    try:    
+        ephem_planet = getattr(ephem, user_text_list[1].lower().capitalize())
+        ephem_planet_time = ephem_planet(datetime.today().strftime('%Y/%m/%d')) 
+        print(ephem.constellation(ephem_planet_time))
+        update.message.reply_text(f"Астрологи предсказали что {user_text_list[1]} в созвездии {ephem.constellation(ephem_planet_time)}")
+    except AttributeError: update.message.reply_text(f"{user_text_list[1]}. Это что за покемон?")
+
+
+ #   if planet.lower() == 'mars':
+  #      mars = ephem.Mars()  
+   #     mars.compute(datetime.today().strftime('%Y/%m/%d'))
+    #    print(ephem.constellation(mars))
+     #   update.message.reply_text(ephem.constellation(mars))
 
 def main():
     mybot = Updater(settings.API_KEY, request_kwargs=settings.PROXY)
 
     logging.info('Бот запускается')
 
-    mars = ephem.Mars()  
-    mars.compute(datetime.today().strftime('%Y/%m/%d'))
-    print(ephem.constellation(mars))
+ #   mars = ephem.Mars()  
+  #  mars.compute(datetime.today().strftime('%Y/%m/%d'))
+   # print(ephem.constellation(mars))
 
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler("start", greet_user))
